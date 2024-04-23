112275c17aeaf5ed22831c48bab72f5269776b4d
AyyasyRosadi <ayyasyrsd@gmail.com>
2024-04-23 11:04:01 +0800

    add warning in form withdraw

--- src/components/Form/FormUpdateWithdrawDisbursementOfFund.tsx
+++ src/components/Form/FormUpdateWithdrawDisbursementOfFund.tsx
@@ -7,8 +7,9 @@ import { useForm } from 'react-hook-form'
 import * as yup from "yup"
 import Form from '../templates/Form'
 import Modal from '../templates/Modal'
+import { IoInformationCircleOutline } from 'react-icons/io5'
 
-export default function FormUpdateWithdrawDisbursementOfFund({ submit, show, close }: { show: boolean, close: () => void, submit: (e: any) => void }):ReactNode {
+export default function FormUpdateWithdrawDisbursementOfFund({ submit, show, close }: { show: boolean, close: () => void, submit: (e: any) => void }): ReactNode {
     const method = useForm({
         mode: 'all',
         resolver: yupResolver(
@@ -21,6 +22,10 @@ export default function FormUpdateWithdrawDisbursementOfFund({ submit, show, clo
     const ptkOptions = usePtkOptions()
     return (
         <Modal title='Update Withdraw Anggaran' show={show} close={close}>
+            <div className='flex gap-2 mb-3'>
+                <IoInformationCircleOutline className='w-8 h-8 text-red-700' />
+                <h1 className='text-red-700 text-sm mt-1'>Peringatan! Pastikan akun dan kalkulasi bulanan untuk antrian ini tersedia dan masih terbuka sebelum menyetujui pengambilan</h1>
+            </div>
             <Form submit={method.handleSubmit(submit)}>
                 <SelectForm instanceId='select_ptk' title='Penerima' method={method} methodName='ptk_id' options={ptkOptions} />
                 <InputForm id='input-receipent' title='Pihak Ketiga (di isi jika yang menerima selain ptk)' method={method} methodName='receipient' />
